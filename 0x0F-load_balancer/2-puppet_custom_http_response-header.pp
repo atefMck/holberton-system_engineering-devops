7d7bc68b4029a3d5fed91fa416a6e76777e82703
atefsama666 <mechken.atef@gmail.com>
2020-04-04 00:04:12 +0100

    Test

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,20 +1,19 @@
 # Configures a server with nginx adding a custom header
-exec { 'apt-get -y update':                                                                                                                                      
-  provider  => 'shell',                                                                                                                                          
-}                                                                                                                                                                
--> package {'check':                                                                                                                                             
-  name => "nginx",                                                                                                                                               
-  ensure => present,                                                                                                                                             
-}                                                                                                                                                                
--> service { 'starting':                                                                                                                                         
-  name => "nginx",                                                                                                                                               
-  ensure => running,                                                                                                                                             
+exec { 'apt-get -y update':
+  provider  => 'shell',
 }
--> exec { 'Adding header':                                                                                                                                          
-  provider  => 'shell',                                                                                                                                          
-  command   => "sed -i -e '/sendfile on;/a \\\tadd_header X-Served-By '$HOSTNAM\                                                                                 
-E';' /etc/nginx/nginx.conf",                                                                                                                                     
-}                                                                                                                                                                
--> exec { 'service nginx restart':                                                                                                                               
-  provider  => 'shell',                                                                                                                                          
-}      
+-> package {'check':
+  name => "nginx",
+  ensure => present,
+}
+-> service { 'starting':
+  name => "nginx",
+  ensure => running,
+}
+-> exec { 'Adding header':
+  provider  => 'shell',
+  command   => "sed -i -e '/sendfile on;/a \\\tadd_header X-Served-By ${hostname};' /etc/nginx/nginx.conf",
+}
+-> exec { 'service nginx restart':
+  provider  => 'shell',
+}
