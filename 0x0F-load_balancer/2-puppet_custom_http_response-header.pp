544ce80f1c111707a6c53273c0040257e4999570
atefsama666 <mechken.atef@gmail.com>
2020-04-03 22:45:33 +0100

    Advanced task

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,16 @@
+# Configures a server with nginx adding a custom header
+exec { 'apt-get -y update':
+  provider  => 'shell',
+}
+exec { 'apt-get -y install nginx':
+  provider  => 'shell',
+}
+exec { 'service nginx start':
+  provider  => 'shell',
+}
+exec { 'sed -i '/sendfile on;/a \\tadd_header X-Served-By '$HOSTNAME';' /etc/nginx/nginx.conf':
+  provider  => 'shell',
+}
+exec { 'service nginx restart':
+  provider  => 'shell',
+}
