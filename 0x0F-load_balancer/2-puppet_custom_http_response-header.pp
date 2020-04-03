ebfffd504a4ea383b1defbaf24829cfd96113a4a
atefsama666 <mechken.atef@gmail.com>
2020-04-03 23:00:51 +0100

    Fixed advanced task

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -2,15 +2,15 @@
 exec { 'apt-get -y update':
   provider  => 'shell',
 }
-exec { 'apt-get -y install nginx':
+-> exec { 'apt-get -y install nginx':
   provider  => 'shell',
 }
-exec { 'service nginx start':
+-> exec { 'service nginx start':
   provider  => 'shell',
 }
-exec { 'sed -i "/sendfile on;/a \\tadd_header X-Served-By "$HOSTNAME";"" /etc/nginx/nginx.conf':
+-> exec { 'sed -i "/sendfile on;/a \\tadd_header X-Served-By "$HOSTNAME";" /etc/nginx/nginx.conf':
   provider  => 'shell',
 }
-exec { 'service nginx restart':
+-> exec { 'service nginx restart':
   provider  => 'shell',
 }
