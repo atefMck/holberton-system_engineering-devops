f96c4befb5140359136c72a57a71b266ae742675
atefsama666 <mechken.atef@gmail.com>
2020-05-29 12:53:15 +0100

    Task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Python Script """
+import requests
+import json
+from sys import argv
+
+if __name__ == "__main__":
+    u_link = "https://jsonplaceholder.typicode.com/users"
+    uid = argv[1]
+
+    u_info = requests.get("{}/{}".format(u_link, uid)).json()
+    e_name = u_info['name']
+
+    todo_info = requests.get("{}/{}/todos".format(u_link, uid)).json()
+    done = list()
+    for todo in todo_info:
+        if todo['completed'] == True:
+            done.append(todo['title'])
+    total = len(todo_info)
+    total_done = len(done)
+
+    string = "Employee {} is done with tasks({}/{}):".format(
+        e_name, total_done, total)
+    print(string)
+    for task in done:
+        print("\t ", task)
