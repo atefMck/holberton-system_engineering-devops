944409028cac87eb0207987c0a77fac9c6b796a7
atefsama666 <mechken.atef@gmail.com>
2020-06-04 10:36:49 +0100

    Task 2 fixed

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,7 +5,7 @@ import requests
 
 def top_ten(subreddit):
     """ Python Method """
-    link = "https://api.reddit.com/r/{}/top".format(subreddit)
+    link = "https://api.reddit.com/r/{}/hot".format(subreddit)
     headers = {
         'User-Agent': 'My User Agent 1.0',
         'From': 'mechken.atef@gmail.com'
@@ -14,7 +14,7 @@ def top_ten(subreddit):
     reddit_req = requests.get(link, headers=headers)
     reddit_info = reddit_req.json()
     try:
-        top_threads = reddit_info["data"]["children"][:11]
+        top_threads = reddit_info["data"]["children"][:10]
         for thread in top_threads:
             print(thread["data"]["title"])
     except Exception as e:
