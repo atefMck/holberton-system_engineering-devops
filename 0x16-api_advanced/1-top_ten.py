63b7889fc55d17975a1b9f690fabe7cebabd4eee
atefsama666 <mechken.atef@gmail.com>
2020-06-04 10:32:43 +0100

    Task 2

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Python Script """
+import requests
+
+
+def top_ten(subreddit):
+    """ Python Method """
+    link = "https://api.reddit.com/r/{}/top".format(subreddit)
+    headers = {
+        'User-Agent': 'My User Agent 1.0',
+        'From': 'mechken.atef@gmail.com'
+    }
+
+    reddit_req = requests.get(link, headers=headers)
+    reddit_info = reddit_req.json()
+    try:
+        top_threads = reddit_info["data"]["children"][:10]
+        for thread in top_threads:
+            print(thread["data"]["title"])
+    except Exception as e:
+        print("None")
